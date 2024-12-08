64a5598ace4a3ce7bb3d5cbf2ed246f4b90d1ba6
Neesui <nanumaya2012@gmail.com>
2024-12-08 22:59:53 +0545

    horizontal Scroll View

--- flutter_training/lib/main.dart
+++ flutter_training/lib/main.dart
@@ -14,23 +14,330 @@ const HomeWidget({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
+        body: SingleChildScrollView(
           child: Column(
             children: [
-              const Text("Hi Nisha",
-          style:TextStyle(fontSize: 50, color: Colors.red)
-          ),
-          Image.network('https://picsum.photos/200',
-            height: 60,
-            width: 60,
+            Container(
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                color: Color(0xff240750),
+                borderRadius: BorderRadius.circular(10)
+              ),
+              width: double.infinity,
+              child: const Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Flutter Class',
+                  style: TextStyle(
+                    fontSize: 40 ,
+                    color: Color(0xffFFFFFF),
+                    fontWeight: FontWeight.w600,
+                  ),
+                  ),
+                  Text('Graphic design is the art and practice of creating visual content to communicate messages effectively. It involves combining typography, imagery, colors, and layout',
+                  style: TextStyle(
+                    fontSize: 10 ,
+                    color: Color(0xffFFFFFF),
+                    fontWeight: FontWeight.w600,
+                  ),
+                  ),
+                ],
+              ),
             ),
-            Image.asset('assets/img.png'),
-            Image.asset('assets/nanu.jpg')
-            ],
-          )
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                color: Color(0xff240750),
+                borderRadius: BorderRadius.circular(10)
+              ),
+              width: double.infinity,
+              child: const Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Flutter Class',
+                  style: TextStyle(
+                    fontSize: 40 ,
+                    color: Color(0xffFFFFFF),
+                    fontWeight: FontWeight.w600,
+                  ),
+                  ),
+                  Text('Graphic design is the art and practice of creating visual content to communicate messages effectively. It involves combining typography, imagery, colors, and layout',
+                  style: TextStyle(
+                    fontSize: 10 ,
+                    color: Color(0xffFFFFFF),
+                    fontWeight: FontWeight.w600,
+                  ),
+                  ),
+                ],
+              ), 
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                color: Color(0xff240750),
+                borderRadius: BorderRadius.circular(10)
+              ),
+              width: double.infinity,
+              child: const Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Flutter Class',
+                  style: TextStyle(
+                    fontSize: 40 ,
+                    color: Color(0xffFFFFFF),
+                    fontWeight: FontWeight.w600,
+                  ),
+                  ),
+                  Text('Graphic design is the art and practice of creating visual content to communicate messages effectively. It involves combining typography, imagery, colors, and layout',
+                  style: TextStyle(
+                    fontSize: 10 ,
+                    color: Color(0xffFFFFFF),
+                    fontWeight: FontWeight.w600,
+                  ),
+                  ),
+                ],
+              ), 
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                color: Color(0xff240750),
+                borderRadius: BorderRadius.circular(10)
+              ),
+              width: double.infinity,
+              child: const Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Flutter Class',
+                  style: TextStyle(
+                    fontSize: 40 ,
+                    color: Color(0xffFFFFFF),
+                    fontWeight: FontWeight.w600,
+                  ),
+                  ),
+                  Text('Graphic design is the art and practice of creating visual content to communicate messages effectively. It involves combining typography, imagery, colors, and layout',
+                  style: TextStyle(
+                    fontSize: 10 ,
+                    color: Color(0xffFFFFFF),
+                    fontWeight: FontWeight.w600,
+                  ),
+                  ),
+                ],
+              ), 
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                color: Color(0xff240750),
+                borderRadius: BorderRadius.circular(10)
+              ),
+              width: double.infinity,
+              child: const Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Flutter Class',
+                  style: TextStyle(
+                    fontSize: 40 ,
+                    color: Color(0xffFFFFFF),
+                    fontWeight: FontWeight.w600,
+                  ),
+                  ),
+                  Text('Graphic design is the art and practice of creating visual content to communicate messages effectively. It involves combining typography, imagery, colors, and layout',
+                  style: TextStyle(
+                    fontSize: 10 ,
+                    color: Color(0xffFFFFFF),
+                    fontWeight: FontWeight.w600,
+                  ),
+                  ),
+                ],
+              ), 
+            ), 
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                color: Color(0xff240750),
+                borderRadius: BorderRadius.circular(10)
+              ),
+              width: double.infinity,
+              child: const Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Flutter Class',
+                  style: TextStyle(
+                    fontSize: 40 ,
+                    color: Color(0xffFFFFFF),
+                    fontWeight: FontWeight.w600,
+                  ),
+                  ),
+                  Text('Graphic design is the art and practice of creating visual content to communicate messages effectively. It involves combining typography, imagery, colors, and layout',
+                  style: TextStyle(
+                    fontSize: 10 ,
+                    color: Color(0xffFFFFFF),
+                    fontWeight: FontWeight.w600,
+                  ),
+                  ),
+                ],
+              ), 
+            ), 
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                color: Color(0xff240750),
+                borderRadius: BorderRadius.circular(10)
+              ),
+              width: double.infinity,
+              child: const Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Flutter Class',
+                  style: TextStyle(
+                    fontSize: 40 ,
+                    color: Color(0xffFFFFFF),
+                    fontWeight: FontWeight.w600,
+                  ),
+                  ),
+                  Text('Graphic design is the art and practice of creating visual content to communicate messages effectively. It involves combining typography, imagery, colors, and layout',
+                  style: TextStyle(
+                    fontSize: 10 ,
+                    color: Color(0xffFFFFFF),
+                    fontWeight: FontWeight.w600,
+                  ),
+                  ),
+                ],
+              ), 
+            ), 
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                color: Color(0xff240750),
+                borderRadius: BorderRadius.circular(10)
+              ),
+              width: double.infinity,
+              child: const Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Flutter Class',
+                  style: TextStyle(
+                    fontSize: 40 ,
+                    color: Color(0xffFFFFFF),
+                    fontWeight: FontWeight.w600,
+                  ),
+                  ),
+                  Text('Graphic design is the art and practice of creating visual content to communicate messages effectively. It involves combining typography, imagery, colors, and layout',
+                  style: TextStyle(
+                    fontSize: 10 ,
+                    color: Color(0xffFFFFFF),
+                    fontWeight: FontWeight.w600,
+                  ),
+                  ),
+                ],
+              ), 
+            ), 
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                color: Color(0xff240750),
+                borderRadius: BorderRadius.circular(10)
+              ),
+              width: double.infinity,
+              child: const Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Flutter Class',
+                  style: TextStyle(
+                    fontSize: 40 ,
+                    color: Color(0xffFFFFFF),
+                    fontWeight: FontWeight.w600,
+                  ),
+                  ),
+                  Text('Graphic design is the art and practice of creating visual content to communicate messages effectively. It involves combining typography, imagery, colors, and layout',
+                  style: TextStyle(
+                    fontSize: 10 ,
+                    color: Color(0xffFFFFFF),
+                    fontWeight: FontWeight.w600,
+                  ),
+                  ),
+                ],
+              ), 
+            ), 
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                color: Color(0xff240750),
+                borderRadius: BorderRadius.circular(10)
+              ),
+              width: double.infinity,
+              child: const Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Flutter Class',
+                  style: TextStyle(
+                    fontSize: 40 ,
+                    color: Color(0xffFFFFFF),
+                    fontWeight: FontWeight.w600,
+                  ),
+                  ),
+                  Text('Graphic design is the art and practice of creating visual content to communicate messages effectively. It involves combining typography, imagery, colors, and layout',
+                  style: TextStyle(
+                    fontSize: 10 ,
+                    color: Color(0xffFFFFFF),
+                    fontWeight: FontWeight.w600,
+                  ),
+                  ),
+                ],
+              ), 
+            ), 
+          ],
+          ),
         ),
       );
   }
-  
 }
 
